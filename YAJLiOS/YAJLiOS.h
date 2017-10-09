5b699e80538c91116525d49879665a4a1bddb956
mb <mb@sic-software.com>
2017-10-09 14:55:10 +0200

    add YAJLiOS framework target

---
+++ YAJLiOS/YAJLiOS.h
@@ -0,0 +1,21 @@
+//
+//  YAJLiOS.h
+//  YAJLiOS
+//
+//  Created by mb on 09.10.17.
+//
+
+#import "NSObject+YAJL.h"
+#import "YAJLGen.h"
+#import "YAJLDocument.h"
+#import "YAJLParser.h"
+
+//! Project version number for YAJLiOS.
+FOUNDATION_EXPORT double YAJLiOSVersionNumber;
+
+//! Project version string for YAJLiOS.
+FOUNDATION_EXPORT const unsigned char YAJLiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YAJLiOS/PublicHeader.h>
+
+
